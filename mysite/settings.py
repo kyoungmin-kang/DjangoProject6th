676cbae85402bc6670343a1421f6c93255f447f5
kyoungmin-kang <134459679+kyoungmin-kang@users.noreply.github.com>
2023-06-26 13:15:30 +0900

    Blog app add

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.Blogconfig'
     'bookmark.apps.BookmarkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
