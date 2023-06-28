a3824dcce766ee032bfc8280ffcac8f24ef8f5fc
kyoungmin-kang <134459679+kyoungmin-kang@users.noreply.github.com>
2023-06-28 10:31:13 +0900

    -

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from bookmark.views import BookmarkLV, BookmarkDV
+from mysite.views import HomeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', HomeView.as_View(), name='home'),
+    path("admin/", admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
     path('blog/', include('blog.urls')),
     path('bookmark/', BookmarkLV.as_view(), name='index'),
     path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
