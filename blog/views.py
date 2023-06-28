b168161027284bfb91424e19bb849ed6c05162e8
kyoungmin-kang <134459679+kyoungmin-kang@users.noreply.github.com>
2023-06-28 10:21:02 +0900

    -

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
-    DayArchiveView, TodayArchiveView
+    DayArchiveView, TodayArchiveView, TemplateView
 
 from blog.models import Post
 
@@ -15,6 +17,15 @@ class PostLV(ListView):
 
 class PostDV(DetailView):
     model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        from django.conf import settings
+        context['disqus_short'] = f"{settings.DISQUS_SHORTNAME}"
+        context['disqus_id'] = f"post-{self.object.id}-{self.object.slug}"
+        context['disqus_url'] = f"{settings.DISQUS_DOMAIN}-{self.object.get_absolute_url()}"
+        context['disqus_title'] = f"{self.object.slug}"
+        return context
 
 
 class PostAV(ArchiveIndexView):
@@ -45,3 +56,20 @@ class PostTAV(TodayArchiveView):
     model = Post
     date_field = 'modify_dt'
     template_name = 'blog/post_archive_day.html'
+
+
+class TagCloudTV(TemplateView):
+    template_name = 'taggit/taggit cloud.html'
+
+
+class TaggedObjectLV(ListView):
+    template_name = 'taggit/taggit post list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__name=self.kwargs.get('tag'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tagname']= self.kwargs['tag']
+        return context
